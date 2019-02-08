bee082ac17a6168572375c0591babd4fb1c530c7
Szymon Kubicki <s.kubicki98@gmail.com>
2019-02-08 12:36:21 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { PlayersInput } from "./components/PlayersInput/PlayersInput"
-
 class App extends Component {
-  state = {
-    schools:[{
-        value: "Uniwesytet Śląski"
-    },{
-        value: "Uniwersytet Ekonomiczny"
-    },{
-        value: "Jakas bardzo dluga nazwa uczelni jaka ktos sobie wymyslil"
-    }],
-  }
 
-
-
-  render() {
-      return (
-        <div className="App">
-            <PlayersInput 
-                addPlayer={this.handlePlayerAdd} 
-                schools={this.state.schools} 
-
-            />
-        </div>
-      );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="mainHeader">
+                    <section className="headerButton">Zawody</section>
+                    <section className="headerButton">Zawodnicy</section>
+                    <section className="headerButton">Solistki</section>
+                    <section className="headerButton">Soliści</section>
+                    <section className="headerButton">Pary</section>
+                    <section className="headerButton">Trójki</section>
+                    <section className="headerButton">Klasyfikacja uczelniana</section>
+                    <section className="headerButton import">Importuj zawody</section>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
